cea29f462d96bdd87eb22e22a1f9bdbf78da84c2
Viktor <wint84@gmail.com>
2023-01-12 09:34:05 +0300

    sprint 14

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.repository;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ItemRepository {
 
