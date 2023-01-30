efa1789336a93a64a44e63eeb113e59e53529ab6
Viktor <wint84@gmail.com>
2023-01-30 16:42:29 +0300

    add-docker

--- server/src/test/java/ru/practicum/shareit/itemTest/ItemControllerMockTest.java
+++ server/src/test/java/ru/practicum/shareit/itemTest/ItemControllerMockTest.java
@@ -14,9 +14,9 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemService;
 
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
